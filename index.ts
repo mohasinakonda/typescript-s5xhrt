dedfbcb5efbf6c9d9e84589352a697509952aa71
mohasinakonda <58334351+mohasinakonda@users.noreply.github.com>
2022-09-25 20:47:29 +0600

    1 file modified

--- index.ts
+++ index.ts
@@ -4,6 +4,8 @@ import './style.css';
 // Write TypeScript code!
 const appDiv: HTMLElement = document.getElementById('app');
 appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
-let name:string
-name='mohasin'
-console.log(name)
+
+function getFullname(firstname: string, lastNane: string) {
+  console.log(firstname + lastNane);
+}
+getFullname('mohasin', '23');
