48cf3af60877856639b6a9128c10352e3944d1b0
mohasinakonda <58334351+mohasinakonda@users.noreply.github.com>
2022-09-25 20:39:39 +0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# typescript-s5xhrt
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/typescript-s5xhrt)
---
+++ index.ts
@@ -0,0 +1,9 @@
+// Import stylesheets
+import './style.css';
+
+// Write TypeScript code!
+const appDiv: HTMLElement = document.getElementById('app');
+appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
+let name:string
+name='mohasin'
+console.log(name)
